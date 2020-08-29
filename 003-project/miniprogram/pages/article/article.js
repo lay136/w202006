e2a29ea78f8f26d902c7501f281eebe054ffcc24
captainchen <1101145957@qq.com>
2020-08-29 17:58:00 +0800

    lifecycle

--- 003-project/miniprogram/pages/article/article.js
+++ 003-project/miniprogram/pages/article/article.js
@@ -5,62 +5,91 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        articles:[
+            {
+                author:'Tom',
+                avatar:'/images/avatar/u1.jpg',
+                time:'10天前',
+                title:'我是文章的标题111',
+                mainImage:'/images/article/a1.jpg',
+                desc:'我是文章描述111',
+                star:20,
+                view:30
+            },
+            {
+                author:'Tom',
+                avatar:'/images/avatar/u2.jpg',
+                time:'3天前',
+                title:'我是文章的标题222',
+                mainImage:'/images/article/a2.jpg',
+                desc:'我是文章描述222',
+                star:20,
+                view:30
+            }
+        ]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        // console.log('article onLoad ...')
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        // console.log('article onReady ...')
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        // console.log('article onShow ...')
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
-
+        // console.log('article onHide ...')
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-
+        // console.log('article onUnload ...')
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-
+        // console.log('article onPullDownRefresh ...')
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        // console.log('article onReachBottom ...')
     },
 
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
-
-    }
+        // console.log('article onShareAppMessage ...')
+    },
+    onPageScroll: function() {
+        // 页面滚动时执行
+        // console.log('article onPageScroll ...')
+    },
+    onResize: function() {
+        // 页面尺寸变化时执行
+        // console.log('article onResize ...')
+    },
 })
--- 003-project/miniprogram/pages/index/index.js
+++ 003-project/miniprogram/pages/index/index.js
@@ -70,8 +70,69 @@ Page({
    wx.navigateTo({
      url:'/pages/article/article'
    })
-   
-  
- }
+ },
+ /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+      // console.log('Index onLoad ...')
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+      // console.log('Index onReady ...')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+      // console.log('Index onShow ...')
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+      // console.log('Index onHide ...')
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+      // console.log('Index onUnload ...')
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+      // console.log('Index onPullDownRefresh ...')
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+      // console.log('Index onReachBottom ...')
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+      // console.log('Index onShareAppMessage ...')
+  },
+  onPageScroll: function() {
+      // 页面滚动时执行
+      // console.log('Index onPageScroll ...')
+  },
+  onResize: function() {
+      // 页面尺寸变化时执行
+      // console.log('Index onResize ...')
+  },
   
 })
